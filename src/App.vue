28fff289c1c706d549001a08ab691759302e9935
Ammar Bin Nasir <majidammar428@gmail.com>
2020-10-06 23:44:13 +0500

    New Component
    Added UserProfile Component

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,16 @@
 <template>
   <div id="app">
-    @{{ user.username }}  - {{ fullName }}
-    <strong>Followers: </strong> {{ followers }}
-    <button @click="followUser">
-      Follow
-    </button>
-
+    <UserProfile/>
   </div>
 </template>
 
 <script>
+import UserProfile from "./components/UserProfile";
 
 export default {
   name: 'App',
-  data() {
-    return{
-      followers: 0,
-      user:{
-        id: 1,
-        username: '_Fanoflix',
-        firstName: 'Ammar',
-        lastName: 'Nasir',
-        email: 'majidammar428@gmail.com',
-        isAdmin: true
-      }
-    }
-  },
-  watch:{
-    followers(newCount, oldCount) {
-      console.log(`Old follower Count = ${oldCount}`)
-      console.log(`New follower Count = ${newCount}`)
-      if (oldCount < newCount)
-      {
-        console.log(`${this.user.username} has gained a new follower!`)
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++
-    }
-  },
-  mounted() {
-    this.followUser();
-  }
+  components: { UserProfile }
+  
 }
 </script>
 
@@ -59,7 +21,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   display: flex;
-  flex-direction: column;
-  text-align: center;
+  min-height: 100vh;
+  background-color: #f3f5fa;
 }
+
 </style>
