aec4839c0459b82a5b84cef7b3535411e07c83f9
Ammar Bin Nasir <majidammar428@gmail.com>
2020-10-07 00:08:30 +0500

    Updated UserProfile Component
    Added 'Admin Badge' without Functionality

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,6 +2,9 @@
     <div class="user-profile">
         <div class="user-profile__user-panel">
             <h1 class="user-profile__username"> @{{ user.username }} </h1>
+                <div class="user-profile__admin-badge">
+                    Admin
+                </div>
                <div class="user-profile__follower-count">
                     <strong>Followers: </strong> {{ followers }}
                </div>
@@ -59,19 +62,40 @@ export default {
     grid-template-columns: 1fr 3fr;
     width: 100%;
     padding: 50px 5%;
+    
 }
 
 .user-profile__user-panel {
+    size: auto;
     display: flex;
     flex-direction: column;
     margin-right: 50px;
-    padding: 20px;
+    padding: 20px 20px;
     background-color: white;    
     border-radius: 5px;
     border: 1px solid #dfe3e8;
+    width: 300px;
+    height: 100px;
 }
 
+.user-profile__username {
+    padding: 10px 0px;
+}
+
+.user-profile__admin-badge {
+    background: rebeccapurple;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    font-weight: bold;
+    padding: 2px 10px;
+}
+
+.user-profile__follower-count {
+    padding: 5px 0px;
+}
 h1 {
+    padding: 10px 0px;
     margin: 0;
 }
 </style>
