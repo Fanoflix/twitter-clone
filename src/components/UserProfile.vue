237435339c86da4f3b3388bfd26e5cccbc5c7725
Ammar Bin Nasir <majidammar428@gmail.com>
2020-10-09 00:46:59 +0500

    Updated UserProfile
    --- UserProfile Component has grown too big. Going to extract the tweet type as a separate component.
    
    --- Added tweet type selector (draft or instant) and the functionality to link the type to the actual content of the tweet.
    
    --- Ugly UI

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,27 +1,51 @@
 <template>
-    <div class="user-profile">
-        <div class="user-profile__user-panel">
-            <h1 class="user-profile__username"> @{{ user.username }} </h1>
-                <div class="user-profile__admin-badge" v-if="user.isAdmin">
-                    Admin
-                </div>
-               <div class="user-profile__follower-count">
-                    <strong>Followers: </strong> {{ followers }}
-               </div>
-               <form class="user-profile__create-tweet">
-                 <label for="newTweet"> <strong> New Tweet </strong></label>
-                 <textarea id="newTweet" rows="4"></textarea>
-               </form>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <!-- Username -->
+      <h1 class="user-profile__username"> @{{ user.username }} </h1>
+        <!-- Admin Badge -->
+        <div class="user-profile__admin-badge" v-if="user.isAdmin">
+            Admin
         </div>
 
-        <div class="user-profile__tweets-wrapper">
-            <TweetItem v-for="tweet in user.tweets" 
-                :key="tweet.id" 
-                :username="user.username" 
-                :tweet="tweet" 
-                @favourite="toggleFavourite"/>
+        <!-- Follower Count Wrapper  -->
+        <div class="user-profile__follower-count">
+            <strong>Followers: </strong> {{ followers }}
         </div>
+
+        <!-- Create Tweet -->
+        <form class="user-profile__create-tweet" @submit.prevent="createNewTweet">
+          <label for="newTweet"> <strong> New Tweet </strong></label>
+          <!-- The value of the "newTweetContent" is now synced up with the value of this text area. This is because we provided a 'v-model' to the text area-->
+          <textarea id="newTweet" rows="4" v-model="newTweetContent"></textarea>
+
+          <!-- Tweet Types Selector UI -->
+          <div class="user-profile__create-tweet-type">
+            <label for="newTweetType"> <strong>Type:</strong></label>
+            <select id="newTweetType" v-model="selectedTweetType">
+              <!-- In Vue, if you put a colon before the attribute. It can then be referenced as a variable -->
+              <option :value="option.value" v-for="(option,index) in tweetTypes" :key="index">
+                {{ option.name }}
+              </option>
+            </select>
+          </div>
+          <!-- A normal button if pressed, will refresh the page. But we don't want that as we're creating a Single Page Application
+               We change this defautl functionality in the obvious place i.e: The form tag. by using @submit.prevent -->
+          <button>
+            Tweet!
+          </button>
+        </form>
     </div>
+
+    <!-- Tweets Wrapper UI -->
+    <div class="user-profile__tweets-wrapper">
+        <TweetItem v-for="tweet in user.tweets" 
+            :key="tweet.id" 
+            :username="user.username" 
+            :tweet="tweet" 
+            @favourite="toggleFavourite"/>
+    </div>
+  </div>
 
 
 </template>
@@ -33,7 +57,13 @@ export default {
     name: "UserProfile",
     components: {TweetItem},
     data() {
-        return {
+      return {
+        newTweetContent: '',
+        selectedTweetType: 'instant',
+        tweetTypes: [
+          { value: 'draft', name: 'Draft'},
+          { value: 'instant', name: 'Instant Tweet'}
+        ],
         followers: 0,
         user: {
             id: 1,
@@ -45,34 +75,42 @@ export default {
             tweets: [
                 {id: 1, content: 'Cloning twitter here *whistles*'},
                 {id: 2, content: 'Testing things out, nothing to see here.'}
-        ]
+            ]
+        }
       }
-    }
-  },
-  watch:{
-    followers(newCount, oldCount) {
-      if (oldCount < newCount)
-      {
-        console.log(`${this.user.username} has gained a new follower!`)
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++
     },
-    toggleFavourite(id) {
-        console.log(`Favourited Tweet #${id}`)
-    }
-  },
-  mounted() {
-    this.followUser();
-  }
+      watch:{
+        followers(newCount, oldCount) {
+          if (oldCount < newCount) {
+            console.log(`${this.user.username} has gained a new follower!`)
+          }
+        }
+      },
+      computed: {
+        fullName() {
+          return `${this.user.firstName} ${this.user.lastName}`
+        }
+      },
+      methods: {
+        followUser() {
+          this.followers++
+        },
+        toggleFavourite(id) {
+          console.log(`Favourited Tweet #${id}`)
+        },
+        createNewTweet() {
+          if (this.newTweetContent && this.selectedTweetType !== 'draft') {
+            // the unshift is a JScript method, that adds new items to the beginning of the array
+            this.user.tweets.unshift({
+              id: this.user.tweets.length + 1,
+              content: this.newTweetContent
+            })
+          }
+        }
+      },
+      mounted() {
+        this.followUser();
+      }
 }
 </script>
 
